7cd1acb104b0e3f07f41f045c0449fae501f0951
webwerks <sandeep.shinde>
2019-01-30 19:04:46 +0530

    permission done

--- app/src/main/java/com/example/sample/Home/HomeActivity.kt
+++ app/src/main/java/com/example/sample/Home/HomeActivity.kt
@@ -1,17 +1,17 @@
 package com.example.sample.Home
 
+import android.Manifest
+import android.widget.Toast
 import com.example.sample.R
 import com.example.sample.base.BaseActivity
 import com.example.sample.databinding.ActivityHomeBinding
-import com.example.sample.permission.PermissionClass
-import javax.inject.Inject
 
 class HomeActivity : BaseActivity<ActivityHomeBinding>() {
+    override fun onPermissionsGranted(requestCode: Int) {
+        Toast.makeText(this,"permission Granted",Toast.LENGTH_SHORT).show()
+    }
 
     lateinit var mBinding: ActivityHomeBinding
-
-    @Inject
-    lateinit var permission: PermissionClass
 
     override fun contentView(): Int = R.layout.activity_home
 
@@ -20,7 +20,8 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>() {
 
         mBinding.btnFile.setOnClickListener {
 
-          permission.permission()
+            requestAppPermissions(arrayOf(Manifest.permission.CALL_PHONE),
+                R.string.runtime_permissions_txt, 101)
         }
     }
 }
--- app/src/main/java/com/example/sample/base/BaseActivity.kt
+++ app/src/main/java/com/example/sample/base/BaseActivity.kt
@@ -1,22 +1,31 @@
 package com.example.sample.base
 
 import android.app.Fragment
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.support.design.widget.Snackbar
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.SparseIntArray
 import android.webkit.WebView
-import com.example.sample.permission.RuntimePermissionsActivity
 import dagger.android.AndroidInjection
 import dagger.android.DaggerActivity
 
 abstract class BaseActivity<in T : ViewDataBinding> : DaggerActivity() {
 
     private lateinit var mBinding: T
+    private var mErrorString: SparseIntArray? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView(this, contentView())
+        mErrorString = SparseIntArray()
         initUI(mBinding)
         WebView(this).settings.allowFileAccess = false
     }
@@ -52,4 +61,65 @@ abstract class BaseActivity<in T : ViewDataBinding> : DaggerActivity() {
         const val ADD_FRAGMENT = 0
         const val REPLACE_FRAGMENT = 1
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        var permissionCheck = PackageManager.PERMISSION_GRANTED
+        for (permission in grantResults) {
+            permissionCheck = permissionCheck + permission
+        }
+        if (grantResults.size > 0 && permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            onPermissionsGranted(requestCode)
+        } else {
+            Snackbar.make(
+                findViewById(android.R.id.content), mErrorString!!.get(requestCode),
+                Snackbar.LENGTH_INDEFINITE
+            ).setAction(
+                "ENABLE"
+            ) {
+                val intent = Intent()
+                intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                intent.addCategory(Intent.CATEGORY_DEFAULT)
+                intent.data = Uri.parse("package:$packageName")
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+                startActivity(intent)
+            }.show()
+        }
+    }
+
+    fun requestAppPermissions(requestedPermissions: Array<String>, stringId: Int, requestCode: Int) {
+
+        mErrorString!!.put(requestCode, stringId)
+        var permissionCheck = PackageManager.PERMISSION_GRANTED
+        var shouldShowRequestPermissionRationale = false
+        for (permission in requestedPermissions) {
+            permissionCheck = permissionCheck + ContextCompat.checkSelfPermission(this, permission)
+            shouldShowRequestPermissionRationale = shouldShowRequestPermissionRationale ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(this, permission)
+        }
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            if (shouldShowRequestPermissionRationale) {
+                Snackbar.make(
+                    findViewById(android.R.id.content), stringId,
+                    Snackbar.LENGTH_INDEFINITE
+                ).setAction(
+                    "GRANT"
+                ) {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        requestedPermissions,
+                        requestCode
+                    )
+                }.show()
+            } else {
+                ActivityCompat.requestPermissions(this, requestedPermissions, requestCode)
+            }
+        } else {
+            onPermissionsGranted(requestCode)
+        }
+    }
+
+    abstract fun onPermissionsGranted(requestCode: Int)
 }
